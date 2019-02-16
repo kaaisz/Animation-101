6ae0031cf718e0f77695d07530a15f2170f927c3
Kaai Suzuki <suzydp@me.com>
2019-02-16 14:05:31 -0800

    Enable reverse condition and loop

--- 03-reverse-and-loop/js/main.js
+++ 03-reverse-and-loop/js/main.js
@@ -3,19 +3,26 @@ $(document).ready(function(){
 	// Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
 
-	// build a scene
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#project01'
-	})
-	.setClassToggle('#project01', 'fade-in') // add class to project01
-	.addIndicators({
-		name: 'fade scene',
-		colorTrigger: 'black',
-		indent: 200,
-		colorStart: '#75C695'
-	}) // this requires a plugin
-	.addTo(controller);
+	// loop through each .project element
+	$('.project').each(function() {
 
+		console.log(this);
+		// build a scene
+		var ourScene = new ScrollMagic.Scene({
+			// this = each project which spacified in $()
+			// children[0] is the img element
+			triggerElement: this.children,
+			triggerHook: 0.9,
+			reverse: false // disable fade-out even if scroll reverse
+		})
+		.setClassToggle(this, 'fade-in') // add class to project01
+		.addIndicators({
+			name: 'fade scene',
+			colorTrigger: 'black',
+			colorStart: '#75C695'
+		}) // this requires a plugin
+		.addTo(controller);
+	});
 });
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Animation-101
+
+http://suzydp.github.io/animation-101/
