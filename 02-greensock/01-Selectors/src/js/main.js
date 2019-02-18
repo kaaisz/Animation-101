e44f6f9fdb2b9f1cc06f4927e36b3dd49c7f4de6
Kaai Suzuki <suzydp@me.com>
2019-02-18 02:41:30 -0800

    Add TweenMax code with Vanilla.js

--- 02-greensock/01-Selectors/src/js/main.js
+++ 02-greensock/01-Selectors/src/js/main.js
@@ -1,14 +1,31 @@
-(function($) {
-  console.log('Hello from tweenmax');
-	// JS code goes here
-	var header = $('#header');
-	TweenLite.to(
-		header, // animate will appear to header,
-		1, // duration will 1 sec, 
-		{
-			// animate with opacity with y offset
-			opacity: 0,
-			y: 50
-		}
-	)
-})(jQuery);
+'use strict';
+console.log('Hello from tweenmax');
+
+// JS code goes here
+// *egs for define html element
+// var header = $('#header'),  // jQuery ID
+// 		h1 = $("h1"),  //jQuery tag
+// 		intro = $(".intro"),  //jQuery class
+// 		firstItem = $("li:first-child"),  //jQuery 1st item
+// 		secondItem = $("li:nth-child(2)"),  //jQuery 2nd item
+// 		lastItem = $("li:last-child");  //jQuery last item
+
+let header = document.querySelector('#header'),
+		h1 = document.querySelector("h1"),
+		intro = document.querySelector(".intro"),
+		firstItem = document.querySelector("li:first-child"),
+		// firstItem = document.getElementsByClassName("list")[0].firstElementChild,
+		secondItem = document.querySelector("li:nth-child(2)"),
+		// secondItem = document.getElementsByClassName("list")[0].children[1],
+		lastItem = document.querySelector("li:last-child");
+		// lastItem = document.getElementsByClassName("list")[0].lastElementChild,
+
+TweenLite.to(
+	header, // animate will appear to header,
+	1, // duration will 1 sec, 
+	{
+		// animate with opacity with y offset
+		opacity: 0,
+		y: 50
+	}
+);
