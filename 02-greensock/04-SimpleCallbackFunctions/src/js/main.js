6b30ce1dcbf73e33ff981e750433c06c55555f52
Kaai Suzuki <suzydp@me.com>
2019-02-18 23:39:04 -0800

    Add code for callback - see how it works

--- 02-greensock/04-SimpleCallbackFunctions/src/js/main.js
+++ 02-greensock/04-SimpleCallbackFunctions/src/js/main.js
@@ -1,9 +1,36 @@
 (function($) {
-    
+
+	// define element
 	var img = $('img'),
-		h2 = $('h2');
+			h2 = $('h2');
+			i = 0;
+
+	// define callback
+	const onStart = () => {
+		console.log('Animation Started');
+	}
+
+	const onUpdate = () => {
+		// console.log('Animation is in Progress');
+		h2.text(i++);
+	}
+
+	const onComplete = () => {
+		console.log('Animation completed!');
+	}
+
 
 	// Simple Callback Functions
-	TweenLite.from(img, 1, {x: -200, ease:Power1.easeInOut});
+	TweenLite.from(
+		img, 
+		1, 
+		{
+			x: -200, 
+			ease:Power1.easeInOut,
+			onStart: onStart,
+			onUpdate: onUpdate,
+			onComplete: onComplete,
+		}
+	);
 
 })(jQuery);
