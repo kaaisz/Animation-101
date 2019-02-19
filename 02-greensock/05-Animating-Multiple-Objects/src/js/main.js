b9f4f9d6e2a2e210c1a6862fd8c6e4241b31bbbb
Kaai Suzuki <suzydp@me.com>
2019-02-19 01:56:30 -0800

    Add code for make timeline to animate orderly

--- 02-greensock/05-Animating-Multiple-Objects/src/js/main.js
+++ 02-greensock/05-Animating-Multiple-Objects/src/js/main.js
@@ -1,11 +1,32 @@
-(function($) {
-    
-	var img = $('img'),
-		h2 = $('h2'),
-		h1 = $('h1'),
-		intro = $('.intro'),
-		listItem = $('ul li');
+'use strict';
+let img = document.querySelector('img'),
+		h2 = document.querySelector('h2'),
+		h1 = document.querySelector('h1'),
+		intro = document.querySelector('.intro'),
+		listItem = document.querySelector('ul'),
+		tl = new TimelineLite();
 
 	// Animating Multiple Objects
+	// fadeout orderly
+	// TweenLite.to(h1, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.2});
+	// TweenLite.to(intro, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.4});
+	// TweenLite.to(img, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.6});
+	// TweenLite.to(h2, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.8});
+	// TweenLite.to(listItem, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 1});
 
-})(jQuery);
+	// fadein orderly
+	// TweenLite.from(h1, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.2});
+	// TweenLite.from(intro, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.4});
+	// TweenLite.from(img, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.6});
+	// TweenLite.from(h2, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 0.8});
+	// TweenLite.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut, delay: 1});
+
+	// tidy up by using tl - timeline
+	// this will appear animation orderly like totally same with above
+	tl
+		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut})
+		// option `-0.15` will make this element animate 0.15sec earlier than defalut
+		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut}, '-=0.15')
+		.from(img, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut}, '-=0.15')
+		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut}, '-=0.15')
+		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease: Power1.easeOut}, '-=0.15');
