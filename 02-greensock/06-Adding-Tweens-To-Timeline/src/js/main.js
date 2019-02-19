75dbbdac71253c509ca8dd727929d680ea0b4d93
Kaai Suzuki <suzydp@me.com>
2019-02-19 02:10:54 -0800

    Add Tweens - absolute position for timeline

--- 02-greensock/06-Adding-Tweens-To-Timeline/src/js/main.js
+++ 02-greensock/06-Adding-Tweens-To-Timeline/src/js/main.js
@@ -1,18 +1,18 @@
-(function($) {
-    
-	var img = $('img'),
-		h2 = $('h2'),
-		h1 = $('h1'),
-		intro = $('.intro'),
-		listItem = $('ul li'),
+'use strict';
+
+let img = document.querySelector('img'),
+		h2 = document.querySelector('h2'),
+		h1 = document.querySelector('h1'),
+		intro = document.querySelector('.intro'),
+		listItem = document.querySelector('ul'),
 		tl = new TimelineLite();
 
-	// Adding Tweens To A Timeline
-	tl
-		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15');
-
-})(jQuery);
+// Adding Tweens To A Timeline
+tl
+	.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+	// after 1 sec from h1, animate will continue
+	.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+	// below will appear after 3sec of total
+	.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+	.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')
+	.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut});
