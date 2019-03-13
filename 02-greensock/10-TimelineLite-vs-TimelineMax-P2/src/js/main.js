f42c62098e622e2adf02c300e6949b62d3fff1df
Kaai Suzuki <suzydp@me.com>
2019-03-12 19:29:01 -0700

    Add further codes for TimelineMax

--- 02-greensock/10-TimelineLite-vs-TimelineMax-P2/src/js/main.js
+++ 02-greensock/10-TimelineLite-vs-TimelineMax-P2/src/js/main.js
@@ -6,13 +6,16 @@
 		intro = $('.intro'),
 		listItem = $('ul li'),
 		buttons = $('button'),
-		tl = new TimelineLite({paused: true}),
+		tl = new TimelineMax({paused: true}),
 		dot = $('.dot'),
 		loader = $('#loader'),
-		tlLoader = new TimelineMax({repeat: 2, onComplete: loadContent});
+		tlLoader = new TimelineMax({repeat: 2, onComplete: loadContent}); 
+		// no need to put {paused} to execute straight away
+		// loadContent will execute after 2 times of repeat ends
 
 	// TimelineLite vs TimelineMax
 	tl
+		.set(header, {autoAlpha: 1}) // to appear main content
 		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
 		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
 		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
@@ -22,19 +25,53 @@
 	// Loader Timeline
 	tlLoader
 		.staggerFromTo(dot, 0.3, 
-			{y: 0, autoAlpha: 0}, 
-			{y: 20, autoAlpha: 1, ease:Back.easeInOut}, 
-			0.05
+			{y: 0, autoAlpha: 0}, // {fromVars}, 
+			{y: 20, autoAlpha: 1, ease: Back.easeInOut},// {toVars}, 
+			0.05 // stagger delay
 		)
 		.fromTo(loader, 0.3, 
-			{autoAlpha: 1, scale: 1.3}, 
-			{autoAlpha: 0, scale: 1, ease:Power0.easeNone},
-			0.9
-		);
+			{autoAlpha: 1, scale: 1.3},// {fromVars} 
+			{autoAlpha: 0, scale: 1, ease: Power0.easeNone},// {toVars}
+			0.9 // absolute position
+		)
 
-	function loadContent(){
-		console.log('bring on the content');
+	function loadContent() {
+		// show the alert for finishing load content
+		let tlLoaderOut = new TimelineLite({onComplete: contentIn});
+		tlLoaderOut
+			.set(
+				dot, 
+				{backgroundColor: '#2b4d66'}
+			)
+			.to(
+				loader, // element
+				0.3, // duration
+				{
+					autoAlpha: 1, 
+					scale: 1.3, // to slightly scale it up
+					ease: Power0.easeNone
+				}
+			)
+			.staggerFromTo(dot, 0.3,
+				{y: 0, autoAlpha: 0},
+				{y: 20, autoAlpha: 1, ease: Back.easeInOut},
+				0.05, 0 
+			)
+			.to(
+				loader, 0.3,
+				{
+					y: -150, // 150px up from the current position
+					autoAlpha: 0, // fade it out
+					ease: Power1.easeIn　// to animate with slide-back-down
+				},
+				'+=0.3'
+			);
 	}
+
+	function contentIn() {
+		tl.play();
+	}
+
 
 	$('#btnPlay').on('click',function(){
 		tl.play();
@@ -73,9 +110,3 @@
 	});
 
 })(jQuery);
-
-
-
-
-
-
